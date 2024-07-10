b84e9aefc5db1cf5ea818b6ff6660f98dea4f518
Parnia-prhn <87763496+Parnia-prhn@users.noreply.github.com>
2024-07-10 09:52:10 +0330

    Loan app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 "use client";
 import HomePage from "./pages/homePage";
+import LoanApp from "./pages/loanApp";
 export default function Home() {
   return (
     <div>
-      <HomePage />
+      {/* <HomePage /> */}
+      <LoanApp />
     </div>
   );
 }
