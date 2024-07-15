4e0f23953d7cc7eab2d883fe4c3293179da8d41a
Parnia-prhn <87763496+Parnia-prhn@users.noreply.github.com>
2024-07-15 10:21:30 +0330

    connect to backend

---
+++ src/app/blogs/[id].tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import BlogsPreviewPage from "../pages/blogsPreviewPage";
+import Blog from "../pages/parts/contents/blog";
+export default function BlogsPreviewPageF() {
+  return (
+    <div>
+      <Blog />
+    </div>
+  );
+}
